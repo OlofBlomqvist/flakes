29b29dd8b5b25d862d0e377924f09d371448d34e
Olof <olof@twnet.se>
2025-03-25 13:29:10 +0100

    save6789

--- flake.nix
+++ flake.nix
@@ -50,7 +50,12 @@
 
         devShells.default = mkxShell {
           name = "DOTNET COMBO-FLAKE";
-          env = dotnetEnv;
+          env = with pkgs.dotnetCorePackages; combinePackages [
+            sdk_6_0
+            sdk_7_0
+            sdk_8_0
+            sdk_9_0
+          ];
         };
 
         devShells.build = pkgs.mkShell {
